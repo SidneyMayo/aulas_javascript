ba53fdc905b79e4a7d47e7f2a74be24254995eff
SidneyMayo <sidneymayo11@gmail.com>
2020-12-19 22:55:31 -0300

    seguimento do curso

---
+++ JS/main.js
@@ -0,0 +1,47 @@
+
+/* var nome = "Sidney Mayo";
+var idade = 27;
+var profissao = "Estudante e aspirante a empreendedor digital"
+alert("Meu nome é " + nome + ", tenho " + idade + " anos e trabalho na " + profissao) */
+
+/*
+var idade = prompt ("Qual  a sua idade")
+if (idade >=18 ){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+} */
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+}; 
+*/
+/*
+var count;
+for(count = 0; count < 5; count ++){
+    alert(count)
+}*/
+
+/*
+var d = new Date();
+alert(d.getDate()+1);
+alert(d.getHours());
+alert(d.getMonth());
+''
+*/
+/*
+function soma(n1, n2){
+return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(novo, novo-nome)
+} */
+
+function clicou(){
+ document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+ //console.log(document.getElementById("agradecimento"))
+}
